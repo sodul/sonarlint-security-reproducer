623745f8070232341c7f38f49450d56714997d50
Stephane Odul <1504511+sodul@users.noreply.github.com>
2024-03-27 15:07:13 -0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,1 +1,6 @@
 """This file is holding fake secrets for the purpose of detection by SonarLint."""
+
+# Example fake key: from:
+# https://community.sonarsource.com/t/sonarlint-for-intellij-5-1-released-aws-secrets-detection-and-other-improvements/46506
+KEY = "AKIAIGKECZXA] AEIJLMQ";
+
